6569f6b8fe1f9e387d3450036aa6314e2cdc893b
cybersecuritydef <155002701+cybersecuritydef@users.noreply.github.com>
2024-08-10 17:33:15 +0300

    Update switchmac.c

--- switchmac.c
+++ switchmac.c
@@ -14,14 +14,14 @@ void help(void){
     printf("-h\tUsing help\n");
     printf("EXAMPLES:\n\t");
     printf("switchmac -r eth0\n\t");
-    printf("switchmac -m 50:2a:14:af:b5:A4 eth0\n");
+    printf("switchmac -s 50:2a:14:af:b5:A4 eth0\n");
 }
 
 int main(int argc, char **argv){
     int opt = 0;
     opterr = false;
     char mac_addr[MAC_SIZE];
-    while((opt = getopt(argc, argv, "r:g:m:h")) != EOF){
+    while((opt = getopt(argc, argv, "r:g:s:h")) != EOF){
         switch(opt){
             case 'r' :
                 if(getmac(mac_addr, MAC_SIZE, argv[2]) != EOF && setmac_rand(argv[2]) != EOF){
